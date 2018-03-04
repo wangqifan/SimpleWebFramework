8d2b0acbc613d1989a134dd92b3fca670f1d4a3f
王起帆 <1694675518@qq.com>
2018-03-04 13:40:44 +0800

    Update routes.py

--- src/routes.py
+++ src/routes.py
@@ -1,15 +1,22 @@
-from src.UserManage import *
 from src.respone import route_static
 
 
 
+#路由装饰器
+def route(url):
+    def decorator(func):
+        route_dict[url]=func
+        return func
+    return decorator
+
+
 #路由表
 route_dict = {
-    '/': Index,
-    '/Index':Index,
-    '/Add':Add,
-    '/Delete':Delete,
-    '/Edit':Edit,
-    'static':route_static,
-    '/Add_Post':Add_Post,
+ #   '/': Index,
+#    '/Index':Index,
+#    '/Add':Add,
+ #   '/Delete':Delete,
+ #   '/Edit':Edit,
+ #   'static':route_static,
+ #   '/Add_Post':Add_Post,
 }
